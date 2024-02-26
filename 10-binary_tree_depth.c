a856223e82bd2d7185bed4fc49ef80db17e04451
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-26 17:34:53 +0000

    TASKS 9 & 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: a pointer to the node to measure the depth
+ * Return: the depth or, 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree && tree->parent)
+	{
+		depth += 1;
+		depth += binary_tree_depth(tree->parent);
+	}
+
+	return (depth);
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure
+ * Return: the height or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+	{
+		height += 1;
+		binary_tree_height(tree->left);
+	}
+	if (tree->right)
+	{
+		height += 1;
+		binary_tree_height(tree->right);
+	}
+
+	return (height);
+}
