7ad114e038fd0655eb7c5e652c40232845949870
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-28 20:07:56 +0000

    TASK 16{MODIFIED}

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,10 +15,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height != right_height)
-		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
-	return (binary_tree_is_full(tree));
+	return (binary_tree_is_full(tree) && left_height == right_height);
 }
 
 /**
