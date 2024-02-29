4af7f04aa26ddc39cbcc4a36d7af8a147a84c77f
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-29 16:32:30 +0000

    TASK 21{MODIFIED}

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -13,30 +13,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_is_full(tree))
-		return 1;
-
 	return (is_complete_recursive(tree, 0, 0, binary_tree_size(tree)));
-}
-
-/**
- * binary_tree_is_full - checks if a binary tree is full
- * @tree: a pointer to the root node of the tree to check
- * Return: 1 if tree is full or 0 if it is not or if tree is NULL
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left)
-			&& binary_tree_is_full(tree->right));
-
-	return (0);
 }
 
 /**
