7eeee70145e421e8a5d6de6445072917987a64a1
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-01 23:15:55 +0000

    TASKS 27 - 28{ADVANCED}

---
+++ 114-bst_remove.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+
+/**
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: a pointer to the root node of the tree where you will remove a node
+ * @value: the value to remove in the tree
+ * Once located, the node containing a value equals to value
+ *     must be removed and freed
+ * If the node to be deleted has two children,
+ *     it must be replaced with its first in-order successor (not predecessor)
+ * Return: a pointer to the new root node of the tree
+ *     after removing the desired value
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *temp;
+
+	if (root == NULL)
+		return (NULL);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL)
+		{
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (root->right == NULL)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+
+		temp = root->right;
+		while (temp->left != NULL)
+			temp = temp->left;
+		root->n = temp->n;
+		root->right = bst_remove(root->right, temp->n);
+	}
+
+	return (root);
+}
